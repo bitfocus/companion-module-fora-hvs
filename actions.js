bb29b23151b1bfa811fd7ad1f911a27a467ac2e1
Michael Allen <Michael.Allen@BarefootChurch.com>
2020-07-03 21:07:59 -0400

    Refactor: Break module into multiple files

---
+++ actions.js
@@ -0,0 +1,155 @@
+let protocol_hvs100 = require("./protocol_hvs100");
+
+let protocol = {
+	...protocol_hvs100,
+};
+
+module.exports = {
+	/**
+	 * Build the list of actions
+	 * @param {string} model - The model we are requesting actions for
+	 * @returns {Object} - The actions
+	 */
+	getActions: (model) => {
+		// Global actions
+		let actions = {};
+		actions["custom"] = {
+			label: "Send Custom Command",
+			options: [
+				{
+					type: "textinput",
+					label: "Command",
+					id: "command",
+					default: "",
+					required: true,
+				},
+			],
+		};
+		actions["reboot"] = { label: "Reboot Switcher" };
+		actions["recall_event"] = {
+			label: "Recall Event",
+			options: [
+				{
+					type: "number",
+					label: "Event Number",
+					id: "event",
+					default: 0,
+					min: 0,
+					max: 99,
+					required: true,
+				},
+			],
+		};
+		actions["reconnect"] = {
+			label: "Reconnect",
+			tooltip:
+				"If the switcher drops the connection, this action will reconnect.",
+		};
+		actions["trans_auto"] = { label: "Auto Transition" };
+		actions["trans_cut"] = { label: "Cut Transition" };
+		actions["xpt_aux"] = {
+			label: "Set AUX",
+			options: [
+				{
+					type: "dropdown",
+					label: "Aux",
+					id: "aux",
+					required: true,
+					default: 1,
+					choices: protocol[model].AUXES,
+				},
+				{
+					type: "dropdown",
+					label: "Source",
+					id: "source",
+					required: true,
+					default: 1,
+					choices: protocol[model].SOURCES_ME.concat(
+						protocol[model].SOURCES_AUX
+					),
+				},
+			],
+		};
+		actions["xpt_me1"] = {
+			label: "Set ME1",
+			options: [
+				{
+					type: "dropdown",
+					label: "Layer",
+					id: "layer",
+					required: true,
+					default: "A",
+					choices: [
+						{ id: "A", label: "A / PGM" },
+						{ id: "B", label: "B / PVW" },
+					],
+				},
+				{
+					type: "dropdown",
+					label: "Source",
+					id: "source",
+					required: true,
+					default: 1,
+					choices: protocol[model].SOURCES_ME,
+				},
+			],
+		};
+
+		return actions;
+	},
+
+	/**
+	 * Process data recieved from the switcher
+	 * @param {string} - The data that was recieved
+	 */
+	dataRecieved: (data) => {
+		// TODO: Process this data to populate feedbacks
+	},
+
+	/**
+	 * Return the command string fot the provided action
+	 * @param {string} model - The model of switcher to get the command for
+	 * @param {string} action - The id of the action we want a command for
+	 * @param {Object} options - Any options from the action
+	 * @returns {string} - The command string
+	 */
+	getCommandForAction: (model, action, options) => {
+		let command = "";
+
+		switch (action) {
+			case "get_state":
+				command = protocol[model].COMMANDS.GET_STATE;
+				break;
+			case "reboot":
+				command = protocol[model].COMMANDS.REBOOT;
+				break;
+			case "recall_event":
+				let eventInt = parseInt(options.event) + 1; // Although the switcher labels them starting at 0, they are recalled with a 1 base...
+				let eventHex = ("0" + eventInt.toString(16)).slice(-2); // The switcher expects the event Id as a 2-digit hexidecimal
+				command = protocol[model].COMMANDS.RECALL_EVENT
+					.replace( "{event}", eventHex);
+				break;
+			case "trans_auto":
+				command = protocol[model].COMMANDS.TRANS_AUTO;
+				break;
+			case "trans_cut":
+				command = protocol[model].COMMANDS.TRANS_CUT;
+				break;
+			case "xpt_me":
+				command = protocol[model].COMMANDS.XPT_ME1
+					.replace("{layer}", options.layer)
+					.replace("{source}", options.source);
+				break;
+			case "xpt_aux":
+				command = protocol[model].COMMANDS.XPT_AUX
+					.replace("{aux}", options.aux)
+					.replace("{source}", options.source);
+				break;
+			case "custom":
+				command = options.command.trim();
+				break;
+		}
+
+		return command;
+	},
+};
---
+++ protocol_hvs100.js
@@ -0,0 +1,66 @@
+module.exports = {
+	HVS100: {
+		COMMANDS: {
+			GET_INPUTS: "GET.SIGNAL_GROUP2",
+			GET_STATE: "GET.ALLDATA_ME_XPT",
+			REBOOT: "CMD.020503",
+			RECALL_EVENT: "CMD.030502{event}", // event[0-99]: 2-digit hex for the selected event
+			TRANS_AUTO: "SET.ME_XPT_ME1_BKGD_TRS_AUTO_STAT:1",
+			TRANS_CUT: "SET.ME_XPT_ME1_BKGD_TRS_AUTO_STAT:3",
+			XPT_ME1: "SET.ME_XPT_ME1_BKGD_{layer}:{source}", // layer: [A=PGM,B=PVW] which layer to set; source: id of the selected source
+			XPT_AUX: "SET.ME_XPT_AUX{aux}:{source}", // aux[1-8]: which aux to set; source: id of the selected source
+		},
+		AUXES: [
+			{ id: 1, label: "Aux 1" },
+			{ id: 2, label: "Aux 2" },
+			{ id: 3, label: "Aux 3" },
+			{ id: 4, label: "Aux 4" },
+			{ id: 5, label: "Aux 5" },
+			{ id: 6, label: "Aux 6" },
+			{ id: 7, label: "Aux 7" },
+			{ id: 8, label: "Aux 8" },
+		],
+		SOURCES_ME: [
+			// Built-in Inputs
+			{ id: 1, label: "Source 1" },
+			{ id: 2, label: "Source 2" },
+			{ id: 3, label: "Source 3" },
+			{ id: 4, label: "Source 4" },
+			{ id: 5, label: "Source 5" },
+			{ id: 6, label: "Source 6" },
+			{ id: 7, label: "Source 7" },
+			{ id: 8, label: "Source 8" },
+			// Optional expansion card inputs
+			{ id: 9, label: "Source 9" },
+			{ id: 10, label: "Source 10" },
+			{ id: 11, label: "Source 11" },
+			{ id: 12, label: "Source 12" },
+			{ id: 13, label: "Source 13" },
+			{ id: 14, label: "Source 14" },
+			{ id: 15, label: "Source 15" },
+			{ id: 16, label: "Source 16" },
+			{ id: 17, label: "Source 17" },
+			{ id: 18, label: "Source 18" },
+			{ id: 19, label: "Source 19" },
+			{ id: 20, label: "Source 20" },
+			// System inputs
+			{ id: 0, label: "Black" },
+			{ id: 29, label: "Still 1" },
+			{ id: 30, label: "Still 2" },
+			{ id: 37, label: "Color Bars" },
+			{ id: 38, label: "Matte 1" },
+			{ id: 39, label: "Matte 2" },
+			{ id: 40, label: "Color Key Fill" },
+			{ id: 41, label: "Color Key Key" },
+			{ id: 42, label: "Sub Effect 1" },
+			{ id: 43, label: "Sub Effect 2" },
+		],
+		SOURCES_AUX: [
+			// Additional AUX-only sources
+			{ id: 46, label: "Program" },
+			{ id: 47, label: "Preview" },
+			{ id: 48, label: "Clean" },
+			{ id: 50, label: "Multi-View" },
+		],
+	},
+};
